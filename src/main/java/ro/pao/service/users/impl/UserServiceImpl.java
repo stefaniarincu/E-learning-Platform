3a004f4f35c0e52969d15f727301cb38c1302b14
stefaniarincu <stefania.rincu@gmail.com>
2023-04-02 12:33:32 +0300

    add function to keep the users sorted by their name

--- src/main/java/ro/pao/service/users/impl/UserServiceImpl.java
+++ src/main/java/ro/pao/service/users/impl/UserServiceImpl.java
@@ -8,7 +8,7 @@ import ro.pao.service.users.UserService;
 import java.util.*;
 
 public class UserServiceImpl implements UserService {
-    private static final Map<UUID, User> userMap = new HashMap<>();
+    private static final Map<UUID, User> userMap = new LinkedHashMap<>();
 
     @Override
     public Map<UUID, User> addAllKindOfUsers() {
@@ -19,6 +19,25 @@ public class UserServiceImpl implements UserService {
         TeacherServiceImpl teacherService = new TeacherServiceImpl();
         Map<UUID, Teacher> teachers = teacherService.getAllItems();
         userMap.putAll(teachers);
+
+        List<Map.Entry<UUID, User>> entries = new ArrayList<>(userMap.entrySet());
+
+        Collections.sort(entries, new Comparator<Map.Entry<UUID, User>>() {
+            public int compare(Map.Entry<UUID, User> e1, Map.Entry<UUID, User> e2) {
+
+                if (e1.getValue().getFirstName().compareTo(e2.getValue().getFirstName()) == 0) {
+                    return e1.getValue().getLastName().compareTo(e2.getValue().getLastName());
+                }
+
+                return e1.getValue().getFirstName().compareTo(e2.getValue().getFirstName());
+            }
+        });
+
+        userMap.clear();
+
+        for (Map.Entry<UUID, User> entry : entries) {
+            userMap.put(entry.getKey(), entry.getValue());
+        }
 
         return userMap;
     }
