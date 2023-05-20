a953ec80eef655e1e6c7597537dcc46aca26822d
stefaniarincu <93484228+stefaniarincu@users.noreply.github.com>
2023-05-20 19:34:23 +0300

    added Logger

---
+++ src/main/java/ro/pao/service/impl/LogServiceImpl.java
@@ -0,0 +1,28 @@
+package ro.pao.service.impl;
+
+import ro.pao.model.records.LoggerRecord;
+import ro.pao.service.LogService;
+
+import java.time.LocalDateTime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class LogServiceImpl implements LogService {
+    private final Logger logger = Logger.getGlobal();
+    private static final LogServiceImpl INSTANCE = new LogServiceImpl();
+    private LogServiceImpl() {
+    }
+    public static LogServiceImpl getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public void log(Level level, String action) {
+        logger.log(level, action);
+    }
+
+    @Override
+    public LoggerRecord logIntoCsv(Level level, String message) {
+        return new LoggerRecord(level, logger.getName(), message, LocalDateTime.now());
+    }
+}
+
